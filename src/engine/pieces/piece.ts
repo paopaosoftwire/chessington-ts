45f5cc3ea5882079f3908cbcde520c80eff62471
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 16:21:40 +0100

    Added refactor notes on Piece

--- src/engine/pieces/piece.ts
+++ src/engine/pieces/piece.ts
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import King from "./king";
 
 export default class Piece {
     public player: Player;
@@ -17,6 +18,22 @@ export default class Piece {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
     }
+
+    //TODO: Add checkPiece function with less dependencies (i.e. newPosition array)
+
+    // public canCapture(possiblePiece: Piece) {
+    //     return (possiblePiece.player !== this.player && !(possiblePiece instanceof King))
+    // }
+    //
+    // public checkPiece(board: Board, position: Square, arrayToUpdate: Square[]) {
+    //     const possiblePiece = board.getPiece(position);
+    //     if (possiblePiece !== undefined) {
+    //         if (this.canCapture(possiblePiece)) {
+    //             arrayToUpdate.push(position)
+    //         }
+    //         return [true, arrayToUpdate]
+    //     }
+    //     return [false, arrayToUpdate]
+    // }
 }
 
-//TODO: Add checkPiece function with less dependencies (i.e. newPosition array)
