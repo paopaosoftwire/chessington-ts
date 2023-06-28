3feb81e79697c9310c24d2f19014ec688ea9abe9
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 10:36:51 +0100

    Passed test for pawns moving one square

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,13 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this); // returns Square.at(row, col)
+        let newPosition;
+        if (this.player === Player.WHITE) {
+            newPosition = Square.at(currentPosition.row + 1, currentPosition.col);
+        } else {
+            newPosition = Square.at(currentPosition.row - 1, currentPosition.col);
+        }
+        return [newPosition];
     }
 }
