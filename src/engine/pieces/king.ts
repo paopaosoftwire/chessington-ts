c4306c6eb5bd74631d3b2b7ef81662b9e217516e
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 12:28:51 +0100

    Passed test for king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this);
+        let newPosition = [];
+
+        let currentRow = currentPosition.row;
+        let currentCol = currentPosition.col;
+
+        const changeFactors = [[0,1],[1,0],[1,1],[-1,-1],[-1,0],[0,-1],[1,-1],[-1,1]];
+
+        for (const factor of changeFactors){
+            const newRow = currentRow+factor[0];
+            const newCol = currentCol+factor[1];
+            if (board.isInBoard(newRow, newCol)){
+                newPosition.push(Square.at(newRow, newCol));
+            }
+        }
+
+        return newPosition;
     }
 }
