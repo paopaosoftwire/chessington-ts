8c42d1fb12b99bf018de265f6fde78a46457929c
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 11:04:54 +0100

    Passed test for bishop movement

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,37 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this); // returns Square.at(row, col)
+        let newPosition = [];
+
+        let currentRow = currentPosition.row;
+        let currentCol = currentPosition.col;
+
+        while (currentRow > 0 && currentCol > 0){
+            newPosition.push(Square.at(--currentRow, --currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow > 0 && currentCol < 7){
+            newPosition.push(Square.at(--currentRow, ++currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow < 7 && currentCol > 0){
+            newPosition.push(Square.at(++currentRow, --currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow < 7 && currentCol < 7){
+            newPosition.push(Square.at(++currentRow, ++currentCol));
+        }
+
+        return newPosition;
     }
 }
