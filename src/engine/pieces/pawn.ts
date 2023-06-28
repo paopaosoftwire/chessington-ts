36eced639b9fa8fa18f4292eb941dc026b98858f
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 10:40:25 +0100

    Passed test for pawns moving two spaces initially

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -10,12 +10,18 @@ export default class Pawn extends Piece {
 
     public getAvailableMoves(board: Board) {
         const currentPosition = board.findPiece(this); // returns Square.at(row, col)
-        let newPosition;
+        let newPosition = [];
         if (this.player === Player.WHITE) {
-            newPosition = Square.at(currentPosition.row + 1, currentPosition.col);
+            if (currentPosition.row === 1){
+                newPosition.push(Square.at(currentPosition.row + 2, currentPosition.col));
+            }
+            newPosition.push(Square.at(currentPosition.row + 1, currentPosition.col));
         } else {
-            newPosition = Square.at(currentPosition.row - 1, currentPosition.col);
+            if (currentPosition.row === 6){
+                newPosition.push(Square.at(currentPosition.row - 2, currentPosition.col));
+            }
+            newPosition.push(Square.at(currentPosition.row - 1, currentPosition.col));
         }
-        return [newPosition];
+        return newPosition;
     }
 }
