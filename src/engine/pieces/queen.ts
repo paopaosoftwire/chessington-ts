ea3e897891c2dad50c54232fbf83af9f44cb1fae
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-28 11:09:04 +0100

    Passed test for queen movement

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,48 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentPosition = board.findPiece(this); // returns Square.at(row, col)
+        let newPosition = [];
+
+        for (let column = 0; column < 8; column++){
+            if (column !== currentPosition.col){
+                newPosition.push(Square.at(currentPosition.row, column));
+            }
+        }
+        for (let row = 0; row < 8; row++){
+            if (row !== currentPosition.row){
+                newPosition.push(Square.at(row, currentPosition.col));
+            }
+        }
+
+        let currentRow = currentPosition.row;
+        let currentCol = currentPosition.col;
+
+        while (currentRow > 0 && currentCol > 0){
+            newPosition.push(Square.at(--currentRow, --currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow > 0 && currentCol < 7){
+            newPosition.push(Square.at(--currentRow, ++currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow < 7 && currentCol > 0){
+            newPosition.push(Square.at(++currentRow, --currentCol));
+        }
+
+        currentRow = currentPosition.row;
+        currentCol = currentPosition.col;
+
+        while (currentRow < 7 && currentCol < 7){
+            newPosition.push(Square.at(++currentRow, ++currentCol));
+        }
+
+        return newPosition;
     }
 }
