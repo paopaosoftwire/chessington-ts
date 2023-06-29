4af175a0e0c10f15ab7fd964bbe5edaacfdfa898
SamYap-Softwire <samyun.yap@softwire.com>
2023-06-29 15:04:45 +0100

    Added and passed a test for pawn promotion

--- tests/engine/pieces/pawn.spec.ts
+++ tests/engine/pieces/pawn.spec.ts
@@ -4,6 +4,8 @@ import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
 import Rook from '../../../src/engine/pieces/rook';
 import King from '../../../src/engine/pieces/king';
+import Queen from '../../../src/engine/pieces/queen';
+
 
 describe('Pawn', () => {
 
@@ -154,6 +156,16 @@ describe('Pawn', () => {
             const piece = board.getPiece(Square.at(3,5));
             (typeof(piece) === 'undefined').should.be.true;
         })
+
+        it('pawn promoted to queen when arrives on the other end', () => {
+            const pawn = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(6,3), pawn);
+            pawn.moveTo(board, Square.at(7,3));
+
+            const piece = board.getPiece(Square.at(7,3));
+
+            (piece instanceof Queen).should.be.true;
+        })
     });
 
     describe('black pawns', () => {
@@ -302,6 +314,16 @@ describe('Pawn', () => {
             board.currentPlayer = Player.WHITE;
             const piece = board.getPiece(Square.at(3,5));
             (typeof(piece) === 'undefined').should.be.true;
+        })
+
+        it('pawn promoted to queen when arrives on the other end', () => {
+            const pawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(1,3), pawn);
+            pawn.moveTo(board, Square.at(0,3));
+
+            const piece = board.getPiece(Square.at(0,3));
+
+            (piece instanceof Queen).should.be.true;
         })
     });
 
